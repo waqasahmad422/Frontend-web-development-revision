45ae66f8fb96e5086521d26ac0e89bb15098d5b5
waqasahmad422 <waqasahmad545818@gmail.com>
2026-04-27 13:58:56 +0500

    ternary operator revision complete with conditonal statement

---
+++ JS/ternary.js
@@ -0,0 +1,11 @@
+// i am starting most use cases in js revision 
+
+// Even and odd program 
+let num = 23;
+let oddEven=(num%2===0)?console.log("number is Even"):console.log("number is Odd");
+
+let marks=50;
+
+let grade=(marks>=90 && marks<=100) ? "Grade A":(marks>=70&& marks<90)? "Grade B":(marks>=50&& marks<70)? "Grade C":'fail';
+
+console.log(grade);
