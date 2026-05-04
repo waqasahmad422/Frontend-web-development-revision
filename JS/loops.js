5ea04e3e7871e199695e4476f45b24f3634a8ecd
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-03 23:43:42 -0700

    some pattran can draw in js

--- JS/loops.js
+++ JS/loops.js
@@ -1,3 +1,40 @@
-for(let i=2; i<=15; i+=2){
-    console.log(i);
+// for (let i = 2; i <= 15; i += 2) {
+//   console.log(i);
+// }
+
+// // for table
+// let num = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(num + " x " + i + " = " + num * i);
+// }
+
+// nested loops
+
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+  console.log(row);
+}
+
+// second pattran
+let n = 1;
+for (let i = 1; i <= 4; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += n;
+    n++;
+}
+console.log(row);
+}
+
+
+
+for(let i=4; i>=1; i--){
+  let row = ""; 
+    for(let j=5; j>=5-i; j--){
+        row += j;
+    }
+    console.log(row);
 }
