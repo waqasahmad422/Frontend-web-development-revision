19e11bbe3698dfcc04e1f23c48bdfaa900340627
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-04 21:16:38 -0700

    some object practice question complete

---
+++ JS/objectPractice.js
@@ -0,0 +1,12 @@
+let dice =Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+
+const car={name:"BMW", model:"X5", year:2020, color:"black"};
+console.log(car.name);
+
+
+const person={name:"John", age:30, city:"New York"};
+person.city="Los Angeles";
+person.country="USA";
+console.log(person);
