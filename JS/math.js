d251a56c5087345128d6e70c71605f1cf29518b9
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-04 20:19:45 -0700

    math function using

---
+++ JS/math.js
@@ -0,0 +1,15 @@
+// let number=6;
+// console.log(Math.pow(number,2));
+// console.log(Math.sqrt(number));
+// console.log(Math.abs(-number));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.8));
+// console.log(Math.round(3.5));
+// console.log(Math.max(1,2,3,4,5));
+// console.log(Math.min(1,2,3,4,5));
+// console.log(Math.random());
+
+
+let num=Math.floor(Math.random()*10)+1;
+
+console.log(num);
