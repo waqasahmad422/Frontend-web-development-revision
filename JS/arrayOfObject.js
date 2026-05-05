d23889451339e4ea699401622116bd7412d7be91
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-04 20:08:12 -0700

    array of object

---
+++ JS/arrayOfObject.js
@@ -0,0 +1,10 @@
+let arrOfObj=[
+    {name:"John", age:30},
+    {name:"Jane", age:25},
+    {name:"Doe", age:35}
+]
+
+console.log(arrOfObj);
+console.log(arrOfObj[0].name); // Accessing the name of the first object
+console.log(arrOfObj[1].age); // Accessing the age of the second object
+console.log(arrOfObj[2].name); // Accessing the name of the third object
