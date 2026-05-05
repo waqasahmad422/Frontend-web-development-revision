500e512a618052655690560bfa9d571a11b242f0
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-04 20:02:53 -0700

    upload

---
+++ JS/object.js
@@ -0,0 +1,23 @@
+// let student = { name: "Waqas Ahmad Shah", age: 20, marks:95.7, grade: "A" , city:"Peshawar"};
+// console.log(student.city="Karachi");
+// console.log(student.gender="Male");
+// delete student.marks;
+// console.log(student);
+
+// nesting of objects
+const info = {
+  waqas: {
+    gander: "male",
+    age: 20,
+  },
+  Kaleem: {
+    gander: "male",
+    age: 19,
+  },
+  tanveer: {
+    gander: "male",
+    age: 21,
+  },
+};
+
+console.log(info.waqas.age);
