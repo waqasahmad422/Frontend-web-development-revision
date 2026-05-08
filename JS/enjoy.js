d5898eac563bf9bf30acb04998b2ce95c8e802ec
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-07 23:10:02 -0700

    some short program in arrow fucntion

---
+++ JS/enjoy.js
@@ -0,0 +1,30 @@
+// let square=(n)=>(n*n);
+// console.log(square(9));
+
+
+// let id=setInterval(() => {
+//     console.log("hello world")
+// }, 2000);
+// console.log(id)
+// setTimeout(() => {
+//     clearInterval(id);
+// }, 12000);
+
+
+// let number=[3,4,2,5,6,7]
+// let avgArr=(arr)=>{
+//     let sum=0;
+//     let avg;
+//     for(let i=0; i<number.length; i++){
+//         sum+=arr[i];
+//     }
+//     return avg=sum/number.length;
+// }
+
+// console.log(avgArr(number));
+
+
+// let even=(n)=> n%2==0;
+
+// console.log(even(4))
+// console.log(even(5))
