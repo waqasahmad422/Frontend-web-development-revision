6ab1506494afaba66d832106c24cb908580ef79d
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-08 04:41:42 -0700

    reduce method in js

--- JS/reduce.js
+++ JS/reduce.js
@@ -3,3 +3,18 @@ let red=num.reduce((res,el)=>(res+el));
 
 console.log(red)
 console.log(typeof(red))
+
+
+
+let arr=[4,5,5,6,7,8,9,0,9,8,7,90,67,102,100];
+
+let maximum = arr.reduce((max,el)=>{
+    if(el>max){
+        return el;
+    }
+    else{
+        return max;
+    }
+});
+
+console.log(maximum)
