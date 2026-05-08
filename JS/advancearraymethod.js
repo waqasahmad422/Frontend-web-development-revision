366055941fd113837b2f6c5dc1adb958506f425e
https://github.com/waqasahmad422 <waqasahmad545818@gmail.com>
2026-05-07 23:35:35 -0700

    foreach concept clear

---
+++ JS/advancearraymethod.js
@@ -0,0 +1,28 @@
+let arr=[1,2,3,4,5,];
+let show=arr.forEach((el)=>{
+    console.log(el)
+})
+
+
+
+let arrOfObject=[
+    {
+        name:"waqas",
+        age:22,
+        city:"Peshawar"
+    },
+    {
+        name:"Ali",
+        age:28,
+        city:"Charsadda"
+    },
+    {
+        name:"Mounser",
+        age:26,
+        city:"Shabqadar"
+    }
+];
+
+arrOfObject.forEach((el)=>{
+    console.log(el)
+})
